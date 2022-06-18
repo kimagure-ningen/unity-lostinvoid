5659465dd31538a6e070634bd743dceefa4e1fa2
Daito <daitoxyamagishi@gmail.com>
2022-06-18 23:32:37 +0900

    fixed rotation

--- Unity TPS/Assets/_Project/Scripts/PlayerAvatar.cs
+++ Unity TPS/Assets/_Project/Scripts/PlayerAvatar.cs
@@ -4,15 +4,67 @@ using UnityEngine;
 
 public class PlayerAvatar : MonoBehaviour
 {
-    private Vector3 latestPos;
-    
-    void Update() {
-        Vector3 diff = transform.position - latestPos;
-        latestPos = transform.position;
+    Quaternion targetRot;
+    Quaternion startRot;
+    Vector3 axis = Vector3.up;
+    float interpolant = 0.2f;
+    float sec;
 
-        if (diff.magnitude > 0.01f)
-        {
-            transform.rotation = Quaternion.LookRotation(diff);
+    void Update() {
+        //! fix here
+        if (Input.GetKey(KeyCode.W)) {
+            Vector3 localAngle = transform.localEulerAngles;
+            localAngle.y = 0f; 
+            transform.localEulerAngles = localAngle;  
+        }
+
+        // if (Input.GetKey(KeyCode.S)) {
+        //     Vector3 localAngle = transform.localEulerAngles;
+        //     localAngle.y = 180f; 
+        //     transform.localEulerAngles = localAngle;  
+        // }
+
+        if (Input.GetKey(KeyCode.S)) {
+            startRot = transform.rotation;
+            targetRot = Quaternion.AngleAxis(180f, axis) * transform.rotation;
+            sec += Time.deltaTime;   
+            transform.rotation = Quaternion.Lerp(startRot, targetRot, sec * interpolant);
+        }
+
+        if (Input.GetKey(KeyCode.A)) {
+            Vector3 localAngle = transform.localEulerAngles;
+            localAngle.y = -90f; 
+            transform.localEulerAngles = localAngle;  
+        }
+        
+        if (Input.GetKey(KeyCode.D)) {
+            Vector3 localAngle = transform.localEulerAngles;
+            localAngle.y = 90f; 
+            transform.localEulerAngles = localAngle;  
+        }
+
+        if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.A)) {
+            Vector3 localAngle = transform.localEulerAngles;
+            localAngle.y = -45f; 
+            transform.localEulerAngles = localAngle;  
+        }
+
+        if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.D)){
+            Vector3 localAngle = transform.localEulerAngles;
+            localAngle.y = 45f; 
+            transform.localEulerAngles = localAngle;
+        }
+
+        if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A)) {
+            Vector3 localAngle = transform.localEulerAngles;
+            localAngle.y = -135f; 
+            transform.localEulerAngles = localAngle;  
+        }
+
+        if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.D)) {
+            Vector3 localAngle = transform.localEulerAngles;
+            localAngle.y = 135f; 
+            transform.localEulerAngles = localAngle;  
         }
     }
 }
